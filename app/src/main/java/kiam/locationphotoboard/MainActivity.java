583e5254fa4d030faa532e3b2412589529d0104f
Kiam <kiamlambellissimo@gmail.com>
2017-01-13 16:43:59 -0500

    Post Class created to anticipate creation of objects.

--- app/src/main/java/kiam/locationphotoboard/MainActivity.java
+++ app/src/main/java/kiam/locationphotoboard/MainActivity.java
@@ -3,6 +3,7 @@ package kiam.locationphotoboard;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 import butterknife.ButterKnife;
---
+++ app/src/main/java/kiam/locationphotoboard/Post.java
@@ -0,0 +1,9 @@
+package kiam.locationphotoboard;
+
+/**
+ * Created by Kiam on 2017-01-13.
+ */
+public class Post
+{
+    int date;
+}
