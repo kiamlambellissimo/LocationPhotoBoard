ff4abd50f2453883302bdde2073403bbb19f493f
duvld <ollejna@gmail.com>
2017-01-13 20:01:44 -0500

    started on the Post Object

--- app/src/main/java/kiam/locationphotoboard/Post.java
+++ app/src/main/java/kiam/locationphotoboard/Post.java
@@ -1,9 +1,87 @@
 package kiam.locationphotoboard;
 
-/**
- * Created by Kiam on 2017-01-13.
- */
-public class Post
-{
-    int date;
+//import java.awt.Image; --what's the image thing?
+import java.util.ArrayList;
+import java.util.Date;
+
+/*	The idea behind this object is to contain everything a certain post will have.
+ * 	For example, if one posts a picture at location X; a Post object will be created
+ * 	to contain the following:
+ *
+ * 	- the Date and Time of post
+ * 	- the Content(1) of the post
+ *	- the Rating(2) of the post
+ *  - TODO: Location stuff
+ *
+ * */
+
+public class Post {
+	/*
+	 * 	Upon initialization of the Post (which happens as soon as the user presses
+	 * 	a button triggering the instantiation withtin the phone app) the following
+	 * 	happens:
+	 *
+	 * 	i) 'theDate' Object is instantiated with the current date
+	 * 	ii) everything within (1) and (2) are initialized to empty; except the Content
+	 * 		itself
+	 *
+	 * 	There exists 2 constructors for the initialization, one for an image post and
+	 * 	another for 'text posts' --put in for testing before images, maybe keep??
+	 *
+	 * */
+
+    private int Rating;
+    private ArrayList<String> Comments;
+
+    // Text post route and Initialization of Content
+    public Post(String textPost) {
+        Date theDate = new Date(); //the date of the moment of uploading
+        testString(textPost); //Limit message to 100 characters, if greater, return error on app.
+
+        // Initialization of (1)
+        Rating = 0; //Initialization of Rating
+        Comments = new ArrayList<String>(); //Initialization of the post's comments.
+
+    }
+
+    //TODO: Initialize image route -- will contain check for size (phone standard), if someone uploads
+    // a camera res photo it will be dumbed down to phone standard resolution
+    /*public Post(Image imagePost) {
+        Date theDate = new Date();
+        Rating = 0;
+        Comments = new ArrayList<String>();
+    }*/
+
+
+    //adding comments
+    public void addToComments(String comment) {
+        Comments.add(comment);
+    }
+
+    //returns the comment array
+    public ArrayList<String> getComments() {
+        return Comments;
+    } //TODO: figure out if Android Studio can use ArrayLists easily
+
+    // add/sub to the rating
+    public void addRating() {
+        Rating++;
+    }
+
+    public void subRating() {
+        Rating--;
+    }
+
+    //returns the rating
+    public int getRating() {
+        return Rating;
+    }
+
+    /*
+     * The Great Limiter
+     * */
+    public static boolean testString(String s) {
+        return s.length() > 100;
+    }
+
 }
