59e348a04ca742613fda43c5da49df3a5a801dce
itgregor <iantgregor@gmail.com>
2017-01-25 15:03:15 -0500

    there u go bitch

--- app/src/main/java/kiam/locationphotoboard/MapTestActivity.java
+++ app/src/main/java/kiam/locationphotoboard/MapTestActivity.java
@@ -1,5 +1,51 @@
 package kiam.locationphotoboard;
 
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MapTestActivity extends Activity implements OnMapReadyCallback {
+
+    final String TAG = "MapTestActivity";
+    final String MAPS_API_KEY = "AIzaSyB1L-ENT2ZdVKYJWZNNYqm-IQAgv0BcSh4";
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+        try {
+            map.setMyLocationEnabled(true);
+        } catch (SecurityException e)
+        {
+            Log.d(TAG, "No permissons");
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        mMap.addMarker(new MarkerOptions().title("Sydney").snippet("The most populous city in Australia.").position(sydney));
+    }
+}
+
+/*
+
+package kiam.locationphotoboard;
+
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +53,8 @@ import android.support.v7.app.AppCompatActivity;
 /**
  * Created by Kiam on 2017-01-12.
  */
-public class MapTestActivity extends AppCompatActivity
+/*
+public class MapTestActivity extends AppCompatActivity implements OnMapReadyCallback{}
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,4 +64,7 @@ public class MapTestActivity extends AppCompatActivity
     }
 
 
+
+
 }
+*/
