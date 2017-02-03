91f54d8d98cb4fa1e61d09ccd95ab50e3cf76da1
Kiam <kiamlambellissimo@gmail.com>
2017-02-03 13:55:18 -0500

    Working on sending object to Backendless. Pushing so Moe can access code.

--- app/src/main/java/kiam/locationphotoboard/CloudTestActivity.java
+++ app/src/main/java/kiam/locationphotoboard/CloudTestActivity.java
@@ -2,6 +2,7 @@ package kiam.locationphotoboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,14 +15,32 @@ public class CloudTestActivity extends AppCompatActivity
     final String YOUR_APP_ID = "A008628F-CA21-B27B-FFB9-3F872AB77900";
     final String YOUR_SECRET_KEY = "A2C5C06C-9C3D-03A5-FF23-8BD591629C00";
     final String appVersion = "v1";
-    final Button mSaveThePostButton = (Button) findViewById(R.id.savePost);
+    final String TAG = "CloudTestActivity";
+    Button mSaveThePostButton;
 
     public void saveNewPost(View view)
     {
-        Post testPost = new Post();
+        final Post testPost = new Post();
         testPost.testString("The Post.");
 
-        Post savedPost =  Backendless.Persistence.save(testPost);
+
+
+       /* Thread t = new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Log.d(TAG, "saveNewPost");
+                try {
+                    Log.d(TAG, "New Post Saved");
+                    Post savedPost =  Backendless.Persistence.save(testPost);
+                } catch (Exception e) {
+                    Log.d(TAG, "New Post Failed");
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.run();*/
 
     }
 
@@ -31,11 +50,14 @@ public class CloudTestActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cloud_test);
-        Backendless.initApp( this, YOUR_APP_ID, YOUR_SECRET_KEY, appVersion );
+        mSaveThePostButton = (Button) findViewById(R.id.savePost);
+        Backendless.initApp(this, YOUR_APP_ID, YOUR_SECRET_KEY, appVersion);
 
-        mSaveThePostButton.setOnClickListener(new View.OnClickListener() {
+        mSaveThePostButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
 
                 saveNewPost(null);
 
