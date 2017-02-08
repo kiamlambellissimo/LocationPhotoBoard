25b2b9d391d6a52ddb861d9ead3e9545b34b2e75
duvld <ollejna@gmail.com>
2017-02-08 17:38:57 -0500

    saving progress

--- app/src/main/java/kiam/locationphotoboard/MapTestActivity.java
+++ app/src/main/java/kiam/locationphotoboard/MapTestActivity.java
@@ -1,16 +1,36 @@
 package kiam.locationphotoboard;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
-public class MapTestActivity extends Activity implements OnMapReadyCallback {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+
+public class MapTestActivity extends Activity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     final String TAG = "MapTestActivity";
     final String MAPS_API_KEY = "AIzaSyBrBtIogaQ2lklgpqhAc3XXmEOqXdI_U4s";
     private GoogleMap mMap;
+    private GoogleApiClient GAC;
+    //private Post p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +45,33 @@ public class MapTestActivity extends Activity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap map) {
         mMap = map;
-
+        //p = new Post("Test");
         LatLng sydney = new LatLng(-33.867, 151.206);
         try {
             map.setMyLocationEnabled(true);
         } catch (SecurityException e)
         {
             Log.d(TAG, "No permissons");
-        }
+        }        mMap.addMarker(new MarkerOptions()
+                        .title("Sydney")
+                        .snippet("Click here to see a Post!")
+                        .position(sydney));
+                        //.setTag());
+                mMap.setOnMarkerClickListener(this);
+
+
+
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
 
-        mMap.addMarker(new MarkerOptions().title("Sydney").snippet("The most populous city in Australia.").position(sydney));
+    }
+
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        //marker.set
+
+        return false;
     }
 }
 
