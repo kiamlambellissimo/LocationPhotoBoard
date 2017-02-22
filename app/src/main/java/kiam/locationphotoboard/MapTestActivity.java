dd66ecb41e569067c1a35b3fb204f89f5d3abeb2
agkoshy <alviskoshy@gmail.com>
2017-02-22 15:57:54 -0500

    Got a Map that loads and asks for permissions before it loads. Need to get the blue dot working.

--- app/src/main/java/kiam/locationphotoboard/MapTestActivity.java
+++ app/src/main/java/kiam/locationphotoboard/MapTestActivity.java
@@ -1,12 +1,18 @@
 package kiam.locationphotoboard;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
+
+import android.Manifest;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -15,98 +21,129 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
+
 //implements OnMapReadyCallback, ConnectionCallback and OnConnectionFailedListener
 //ConnectionCallback provides callbacks when user is connected/disconnected
 //OnConnectionFailedListener provides callbacks when a failed connection attempt is made by the client to the server
 public class MapTestActivity extends Activity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    final String TAG = "MapTestActivity";
-    final String MAPS_API_KEY = "AIzaSyBrBtIogaQ2lklgpqhAc3XXmEOqXdI_U4s";
-    private GoogleMap mMap;
+    private static final int FINE_LOCATION_PERMISSION_REQUEST = 1;
+    private static final int CONNECTION_RESOLUTION_REQUEST = 2;
     private GoogleApiClient mGoogleApiClient;
-    protected Location mLastLocation;
-
-    public String mLatitudeText;
-    public String mLongitudeText;
-    private FusedLocationProviderApi locationProvider;
-    private static final int MY_PERMISSION_ACCESS_COARSE_LOCATION = 11;
-
-    public MapTestActivity() {
-    }
+    private GoogleMap mMap;
+    private Location mLastLocation;
+    private String mLatitudeText;
+    private String mLongitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        //creates a mapfragment
+
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        //This statement basically lets us access GoogleApiClient and that allows us to do things like
-        // let us have callbacks that let us use methods that tell the user if they are
-        // connected/disconnected/suspended etc.
+
+        buildGoogleAPIClient();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        buildGoogleAPIClient();
+    }
+
+    private void buildGoogleAPIClient() {
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addApi(LocationServices.API)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
                     .build();
         }
     }
-    //Connects the user to the GoogleApiClient on start
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        findLocation();
+    }
+
     protected void onStart() {
         mGoogleApiClient.connect();
         super.onStart();
     }
 
-    //Disconnects the user from the GoogleApiClient after closing
     protected void onStop() {
         mGoogleApiClient.disconnect();
         super.onStop();
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
-        mMap = map;
-
-
-        try {
-            map.setMyLocationEnabled(true);
-        } catch (SecurityException e)
-        {
-            Log.d(TAG, "No permissons");
-        }
-
-
-       // mMap.addMarker(new MarkerOptions().title("Sydney").snippet("The most populous city in Australia.").position(toronto));
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-       //Asks the user for permission to use the GPS
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_ACCESS_COARSE_LOCATION);
-        }
-        //gets last location of user
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);;
-        mMap.getUiSettings().setMyLocationButtonEnabled(false);
-        LatLng latlng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 13));
-        if (mLastLocation != null) {
-            mLatitudeText = String.valueOf(mLastLocation.getLatitude());
-            mLongitudeText = String.valueOf(mLastLocation.getLongitude());
-        }
-
-    }
-    //needs to be implemented but this is where we throw stuff if the connection is suspended
     @Override
     public void onConnectionSuspended(int i) {
+        Toast.makeText(this, "Connection suspended", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull final ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(this, CONNECTION_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                // There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
+            }
+        } else {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), this, 1);
+            dialog.show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CONNECTION_RESOLUTION_REQUEST && resultCode == RESULT_OK) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    private void findLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    FINE_LOCATION_PERMISSION_REQUEST);
+        } else {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            LatLng latlng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 13));
+            if (mLastLocation != null) {
+                mLatitudeText = String.valueOf(mLastLocation.getLatitude());
+                mLongitudeText = String.valueOf(mLastLocation.getLongitude());
+            }
+        }
 
     }
-    //needs to be implemented but this is where we throw stuff if the connection failed
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case FINE_LOCATION_PERMISSION_REQUEST: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    findLocation();
+                }
+            }
+        }
     }
 }
-
