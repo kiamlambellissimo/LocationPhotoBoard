a2726872e05ae0a01e16a58cdfd5783d72c423d3
duvld <ollejna@gmail.com>
2017-03-02 19:24:58 -0500

    test commit 3

---
+++ app/src/main/java/kiam/locationphotoboard/TestingShit.java
@@ -0,0 +1,21 @@
+package kiam.locationphotoboard;
+
+/**
+ * Created by angietong on 2017-03-02.
+ */
+
+public class TestingShit {
+
+    private int nothing;
+    private String nothing2;
+
+    TestingShit(int n, String s)
+    {
+        if (n > 100)
+        {
+            s = new Integer(n).toString(); //i do nothing
+
+        }
+    }
+
+}
